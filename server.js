5ecb62dafe8c701829929e4ea9309ccd997bcdd7
Mario Mariani <marioslmariani@gmail.com>
2017-03-31 19:05:35 -0300

    Get dist folder from .env

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var app = express();
 app.use(bodyParser.json());
 
 // Create link to Angular build directory
-var distDir = __dirname + '/.tmp/';
+var distDir = __dirname + (process.env.DIST_FOLDER || '/.tmp/');
 app.use(express.static(distDir));
 
 // Initialize the app.
-var server = app.listen(process.env.PORT || 8000, function () {
+var server = app.listen(process.env.PORT || 8000, function() {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
