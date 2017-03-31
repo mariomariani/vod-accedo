eaba4d2e246dc774e41af4598cc3d9677f4ad9a8
Mario Mariani <marioslmariani@gmail.com>
2017-03-31 18:47:33 -0300

    Add server to serve app from heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+// Create link to Angular build directory
+var distDir = __dirname + '/.tmp/';
+app.use(express.static(distDir));
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8000, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
